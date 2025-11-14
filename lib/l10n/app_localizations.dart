2c5c0851d7f3eb020cc4bf37f5e8a36e7c9ccaf8
peter.fux <peter.fux@willhaben.at>
2025-11-14 23:12:58 +0100

    introduce auto-versioning

---
+++ lib/l10n/app_localizations.dart
@@ -0,0 +1,169 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart' as intl;
+
+import 'app_localizations_de.dart';
+import 'app_localizations_en.dart';
+
+// ignore_for_file: type=lint
+
+/// Callers can lookup localized strings with an instance of DraftModeLocalizations
+/// returned by `DraftModeLocalizations.of(context)`.
+///
+/// Applications need to include `DraftModeLocalizations.delegate()` in their app's
+/// `localizationDelegates` list, and the locales they support in the app's
+/// `supportedLocales` list. For example:
+///
+/// ```dart
+/// import 'l10n/app_localizations.dart';
+///
+/// return MaterialApp(
+///   localizationsDelegates: DraftModeLocalizations.localizationsDelegates,
+///   supportedLocales: DraftModeLocalizations.supportedLocales,
+///   home: MyApplicationHome(),
+/// );
+/// ```
+///
+/// ## Update pubspec.yaml
+///
+/// Please make sure to update your pubspec.yaml to include the following
+/// packages:
+///
+/// ```yaml
+/// dependencies:
+///   # Internationalization support.
+///   flutter_localizations:
+///     sdk: flutter
+///   intl: any # Use the pinned version from flutter_localizations
+///
+///   # Rest of dependencies
+/// ```
+///
+/// ## iOS Applications
+///
+/// iOS applications define key application metadata, including supported
+/// locales, in an Info.plist file that is built into the application bundle.
+/// To configure the locales supported by your app, you’ll need to edit this
+/// file.
+///
+/// First, open your project’s ios/Runner.xcworkspace Xcode workspace file.
+/// Then, in the Project Navigator, open the Info.plist file under the Runner
+/// project’s Runner folder.
+///
+/// Next, select the Information Property List item, select Add Item from the
+/// Editor menu, then select Localizations from the pop-up menu.
+///
+/// Select and expand the newly-created Localizations item then, for each
+/// locale your application supports, add a new item and select the locale
+/// you wish to add from the pop-up menu in the Value field. This list should
+/// be consistent with the languages listed in the DraftModeLocalizations.supportedLocales
+/// property.
+abstract class DraftModeLocalizations {
+  DraftModeLocalizations(String locale)
+    : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+
+  final String localeName;
+
+  static DraftModeLocalizations? of(BuildContext context) {
+    return Localizations.of<DraftModeLocalizations>(
+      context,
+      DraftModeLocalizations,
+    );
+  }
+
+  static const LocalizationsDelegate<DraftModeLocalizations> delegate =
+      _DraftModeLocalizationsDelegate();
+
+  /// A list of this localizations delegate along with the default localizations
+  /// delegates.
+  ///
+  /// Returns a list of localizations delegates containing this delegate along with
+  /// GlobalMaterialLocalizations.delegate, GlobalCupertinoLocalizations.delegate,
+  /// and GlobalWidgetsLocalizations.delegate.
+  ///
+  /// Additional delegates can be added by appending to this list in
+  /// MaterialApp. This list does not have to be used at all if a custom list
+  /// of delegates is preferred or required.
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
+        delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ];
+
+  /// A list of this localizations delegate's supported locales.
+  static const List<Locale> supportedLocales = <Locale>[
+    Locale('de'),
+    Locale('en'),
+  ];
+
+  /// No description provided for @btnCancel.
+  ///
+  /// In en, this message translates to:
+  /// **'Cancel'**
+  String get btnCancel;
+
+  /// No description provided for @btnDelete.
+  ///
+  /// In en, this message translates to:
+  /// **'Delete'**
+  String get btnDelete;
+
+  /// No description provided for @btnConfirmNo.
+  ///
+  /// In en, this message translates to:
+  /// **'No'**
+  String get btnConfirmNo;
+
+  /// No description provided for @btnConfirmYes.
+  ///
+  /// In en, this message translates to:
+  /// **'Yes'**
+  String get btnConfirmYes;
+
+  /// No description provided for @workerAutoConfirmIn.
+  ///
+  /// In en, this message translates to:
+  /// **'Automatically confirms in {time}'**
+  String workerAutoConfirmIn({required Object time});
+}
+
+class _DraftModeLocalizationsDelegate
+    extends LocalizationsDelegate<DraftModeLocalizations> {
+  const _DraftModeLocalizationsDelegate();
+
+  @override
+  Future<DraftModeLocalizations> load(Locale locale) {
+    return SynchronousFuture<DraftModeLocalizations>(
+      lookupDraftModeLocalizations(locale),
+    );
+  }
+
+  @override
+  bool isSupported(Locale locale) =>
+      <String>['de', 'en'].contains(locale.languageCode);
+
+  @override
+  bool shouldReload(_DraftModeLocalizationsDelegate old) => false;
+}
+
+DraftModeLocalizations lookupDraftModeLocalizations(Locale locale) {
+  // Lookup logic when only language code is specified.
+  switch (locale.languageCode) {
+    case 'de':
+      return DraftModeLocalizationsDe();
+    case 'en':
+      return DraftModeLocalizationsEn();
+  }
+
+  throw FlutterError(
+    'DraftModeLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.',
+  );
+}
