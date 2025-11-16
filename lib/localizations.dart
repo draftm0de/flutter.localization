e5d6d2cadbf44ecb50632c5c7d08c361c3a4e6c6
peter.fux <peter.fux@willhaben.at>
2025-11-16 22:09:50 +0100

    last reviews to be usable in other repos

--- lib/localizations.dart
+++ lib/localizations.dart
@@ -1,4 +1,3 @@
-library draftmode_localization;
 /// Public entry point that re-exports the generated DraftMode localization
 /// delegate so consumers only depend on `package:draftmode_localization`.
 export 'l10n/app_localizations.dart';
